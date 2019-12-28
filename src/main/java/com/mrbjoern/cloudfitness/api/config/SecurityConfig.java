e4993bd3af8786cf1cef4fd5fe0c6c7cf795348c
Halvor Bjørn <halvor.bjorn@nav.no>
2019-12-28 12:54:59 +0100

    Adds OAuth Resource server support for API

--- src/main/java/com/mrbjoern/cloudfitness/api/config/SecurityConfig.java
+++ src/main/java/com/mrbjoern/cloudfitness/api/config/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -12,10 +16,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login((Customizer.withDefaults()))
+                //.oauth2Login(oauth2login ->
+                //        oauth2login.userInfoEndpoint(userInfoEndpointConfig ->
+                //                userInfoEndpointConfig.oidcUserService(this.oidcUserService())))
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
                 .antMatchers("/api/**").authenticated().and()
                 .oauth2ResourceServer().jwt();
+    }
+
+    private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+        return new OidcUserService();
     }
 }
